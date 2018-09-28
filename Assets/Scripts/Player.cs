29a28f418f0a012c9c73490b537b5b1c246a28af
ellier01234 <43067363+ellier01234@users.noreply.github.com>
2018-09-28 16:21:54 +0100

    Added assets to players menu

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    // designer variables 
+    public float speed = 10;
+    public Rigidbody2D physicsBody;
+    public string horizontalAxis = ("Horizontal");
+    public string verticaAxis = ("Vertical;");
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // get axis input from unity 
+        float leftRight = Input.GetAxis("Horizontal");
+        float upDown = Input.GetAxis("Vertical");
+
+        // create direction vector from axis input 
+        Vector2 direction = new Vector2(leftRight, upDown);
+
+        // make direction vector length 1 
+        direction = direction.normalized;
+
+        // calculate velocity 
+        Vector2 velocity = direction * speed;
+
+        // give the velocity to the rigid body 
+        physicsBody.velocity = velocity;
+	}
+}
